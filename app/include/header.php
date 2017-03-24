f2b42ea3e484c59ae1af4073d3a022bbc2e1ae42
unknown <you@example.com>
2017-03-24 23:42:59 +0300

    header

--- app/include/header.php
+++ app/include/header.php
@@ -1,1 +1,58 @@
-header.php
+<header class="az-header-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="clearfix">
+					<div class="pull-left">
+						<div class="az-menu">
+							<a href="#" class="az-menu-icon hidden-sm hidden-md hidden-lg"></a>
+							<ul>
+								<li><a href="#">События</a></li>
+								<li><a href="#">Статьи</a></li>
+								<li><a href="#">Каталог поселков</a></li>
+								<li><a href="#">тарифы</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="pull-right">
+						<div class="az-sign-up hidden-xs">
+							<a href="#">войти как покупатель</a>
+							<a href="#">войти как продавец</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<section class="az-header-bottom">
+	<div class="container">
+		<div class="az-dashed">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+					<div class="az-logo">
+						<img src="img/logo.png" alt="100 земель">
+					</div>
+				</div>
+				<div class="hidden-xs col-sm-3 col-md-4 col-lg-4">
+					<div class="az-select">
+						<span class="az-baloon"></span>
+						<select name="" id="">
+							<option value="Ярославская область">Ярославская область</option>
+							<option value="Московская область">Московская область</option>
+							<option value="Тверская область">Тверская область</option>
+						</select>
+					</div>
+				</div>
+				<div class="hidden-xs col-sm-5 col-md-5 col-lg-5">
+					<div class="az-search">
+						<form action="/">
+							<input type="text" name="s" placeholder="Быстрый поиск">
+							<input type="submit" value="">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
