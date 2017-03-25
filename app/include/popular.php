0411e7166caccb5ae6ff634c934d2f68de384156
ahatuhov <ahatuhov@gmail.com>
2017-03-25 13:39:25 +0300

    123block and popular box

--- app/include/popular.php
+++ app/include/popular.php
@@ -1,1 +1,108 @@
-popular.php
+<section class="ah-popular">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<span class="ah-popular-title">Популярные объекты</span>
+	    	</div>
+	    	<div class="clearfix"></div>
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix hidden-lg hidden-md hidden-xs"></div>
+			<div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
+				<div class="ah-popularwrapp">
+					<div class="ah-popular-imgbox">
+						<a href="#"><img src="img/ah-popular.jpg" class="img-responsive" alt="ah-popular"></a>
+					</div>
+					<div class="ah-popular-box">
+						<span class="ah-popular-subtitle">Участок, 16 соток</span>
+						<p class="ah-popular-text">ж/д ст. Приветненское, 1.40 км поселок Приветнинское, ДНП Горная Дубрава</p>
+						<span class="ah-popular-price">120 000 р.</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
