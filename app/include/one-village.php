98f23e159b2eea5b2ee7baa739f2227bce240a82
iwebnal <iwebnal@gmail.com>
2017-03-30 17:59:39 +0300

    one valleg

--- app/include/one-village.php
+++ app/include/one-village.php
@@ -6,11 +6,58 @@
 					<div class="ah-onevillage-imgbox">
 						<img src="img/ah-onevellag.jpg" class="img-responsive" alt="onevellag">
 					</div>
-					<span></span>
+					<span class="ah-onevillage-span">Просмотров: 19</span>
 				</div>
 			</div>
 			<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-				
+				<div class="ah-onevillage-wrapp1">
+					<span class="ah-onevillage-subtitle">Описание:</span>
+					<ul class="ah-onevillage-list">
+						<li><a href="#" class="ah-onevillage-link">Добавить в избранное</a></li>
+						<li><a href="#" class="ah-onevillage-link1">Это мой объект</a></li>
+					</ul>
+				</div>
+				<div class="ah-onevillage-wrapp1">
+					<span class="ah-onevillage-span1">Удобный поселок расположенный в заволжском районе. В поселке есть электричество.</span>
+					<p class="ah-onevillage-text">В настоящее время закончено строительство линии электропередач и строительство дорог внутри поселка. Имеется возможность газификации. Первая очередь уже газифицирована. ШРП на границе участка. В настоящее время закончена внутренняя разводка газопровода и по второй очереди. Удачное место для постоянного места жительства: близость города, транспортная доступность, рядом жилые районы города.</p>
+				</div>
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+						<div class="ah-onevillage-rowbox ah-vilrowbox-color">
+							<span class="ah-onevillist1-span">Вода:</span>
+							<span class="ah-onevillist1-span1">Есть</span>
+						</div>
+						<div class="ah-onevillage-rowbox">
+							<span class="ah-onevillist1-span">Электричество:</span>
+							<span class="ah-onevillist1-span1">Есть</span>
+						</div>
+						<div class="ah-onevillage-rowbox ah-vilrowbox-color">
+							<span class="ah-onevillist1-span">Газ:</span>
+							<span class="ah-onevillist1-span1">Нет</span>
+						</div>
+						<div class="ah-onevillage-rowbox">
+							<span class="ah-onevillist1-span">Канализация:</span>
+							<span class="ah-onevillist1-span1">Есть</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<ul class="ah-onevillage-firstlist">
+					<li>
+						<span class="ah-onefirstlist-span ah-onevilcolor1"></span>
+						<span class="ah-onefirstlist-span1">Участок свободен</span>
+					</li>
+					<li>
+						<span class="ah-onefirstlist-span ah-onevilcolor2"></span>
+						<span class="ah-onefirstlist-span1">Участок продан</span>
+					</li>
+					<li>
+						<span class="ah-onefirstlist-span ah-onevilcolor3"></span>
+						<span class="ah-onefirstlist-span1">Участок забронирован</span>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
