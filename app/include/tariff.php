c6d40c9b2f9413045c08bb2c1d8d22912a028a08
hsrazna <anzar.terek@gmail.com>
2017-04-02 11:45:03 +0300

    tariff table mobile

---
+++ app/include/tariff.php
@@ -0,0 +1,249 @@
+<section class="az-tariff">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h2 class="style2"><span>Тарифные планы</span></h2>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="az-tariff-tables">
+					<div class="az-tarif-table">
+						<div class="az-tt-row">
+							<div class="az-tt-head">старт</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа земельного участка</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа коттеджных поселков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Ценовые категории учестков</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Виджет интерактивной карты<br>на свой сайт</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Передача заявок по e-mail</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Базовая аналитика</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Расширенная аналитика</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Выгрузка заявок в CRM<br>(AmoCRM, Bitrix24)</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Конструктор акций</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Доступ для агентов</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Реферальные ссылки</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Правила резервирования участков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-foot">
+								<div class="az-tt-price az-green">Бесплатно</div>
+								<div class="az-tt-bye"><a href="#">подключиться</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="az-tarif-table">
+						<div class="az-tt-row">
+							<div class="az-tt-head">БИЗНЕСС</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа земельного участка</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа коттеджных поселков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Ценовые категории учестков</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Виджет интерактивной карты<br>на свой сайт</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Передача заявок по e-mail</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Базовая аналитика</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Расширенная аналитика</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Выгрузка заявок в CRM<br>(AmoCRM, Bitrix24)</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Конструктор акций</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Доступ для агентов</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Реферальные ссылки</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Правила резервирования участков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-foot">
+								<div class="az-tt-price">1 999 р.</div>
+								<div class="az-tt-bye"><a href="#">подключиться</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="az-tarif-table">
+						<div class="az-tt-row">
+							<div class="az-tt-head az-tt-soon">ПРЕМИУМ</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа земельного участка</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа коттеджных поселков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Ценовые категории учестков</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Виджет интерактивной карты<br>на свой сайт</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Передача заявок по e-mail</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Базовая аналитика</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Расширенная аналитика</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Выгрузка заявок в CRM<br>(AmoCRM, Bitrix24)</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Конструктор акций</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Доступ для агентов</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Реферальные ссылки</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Правила резервирования участков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-foot">
+								<div class="az-tt-price">4 999 р.</div>
+								<div class="az-tt-bye"><a href="#">подключиться</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="az-tarif-table">
+						<div class="az-tt-row">
+							<div class="az-tt-head az-tt-soon">АГЕНТ</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа земельного участка</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Продажа коттеджных поселков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Ценовые категории учестков</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Виджет интерактивной карты<br>на свой сайт</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Передача заявок по e-mail</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Базовая аналитика</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Расширенная аналитика</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Выгрузка заявок в CRM<br>(AmoCRM, Bitrix24)</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Конструктор акций</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Доступ для агентов</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Реферальные ссылки</span></div>
+							<div class="az-tt-right">∞</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-left"><span>Правила резервирования участков</span></div>
+							<div class="az-tt-right">1</div>
+						</div>
+						<div class="az-tt-row">
+							<div class="az-tt-foot">
+								<div class="az-tt-price">999 р.</div>
+								<div class="az-tt-bye"><a href="#">подключиться</a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
