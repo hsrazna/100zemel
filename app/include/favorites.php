703a1af6e48fcfdc48f6ac093ac0d15d8f3837f8
hsrazna <anzar.terek@gmail.com>
2017-04-02 15:03:04 +0300

    favorites begin

---
+++ app/include/favorites.php
@@ -0,0 +1,23 @@
+<section class="az-favorites">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h2 class="style2"><span>Избранное</span></h2>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="az-favorites-wrap">
+					<div class="az-favorite-item">
+						<div class="az-fi-del"><a href="#" class="az-del-gal"></a></div>
+						<div class="az-fi-img">
+							<img src="img/ah-onevellag.jpg" alt="участок">
+						</div>
+						<div class="az-fi-cont">
+							<div class="az-fi-title">Коттеджный поселок «Отрадное»</div>
+							<div class="az-fi-subtitle">Участок №19</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
