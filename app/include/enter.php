4b7a03af7edcef0165427a41c6e5955385983a0e
hsrazna <anzar.terek@gmail.com>
2017-04-10 12:27:38 +0300

    register login

---
+++ app/include/enter.php
@@ -0,0 +1,36 @@
+<section class="az-enter">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="az-enter-wrap">
+					<div class="az-enter-inner">
+						<div class="az-enter-title">Вход</div>
+						<div class="az-enter-section">
+							<div class="az-enter-sec-left"><label for="">Логин:</label></div>
+							<div class="az-enter-sec-right"><input type="text" placeholder="Введите логин"></div>
+						</div>
+						<div class="az-enter-section">
+							<div class="az-enter-sec-left"><label for="">Пароль:</label></div>
+							<div class="az-enter-sec-right"><input type="password" placeholder="Введите пароль"></div>
+						</div>
+						<div class="az-enter-section az-right">
+							<div class="az-enter-check"><input type="checkbox" id="az-enter-checkbox"><label for="az-enter-checkbox"><span></span>Сохранить пароль</label></div>
+						</div>
+						<div class="az-enter-section az-center">
+							<input type="submit" value="вход">
+						</div>
+						<div class="az-enter-soc">
+							<div class="az-enter-soc-title">Войти через соц. сети:</div>
+							<div class="az-enter-soc-inner">
+								<a href="#" class="az-ok"></a>
+								<a href="#" class="az-inst"></a>
+								<a href="#" class="az-fb"></a>
+								<a href="#" class="az-vk"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
