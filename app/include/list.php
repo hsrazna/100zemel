791b980d61277e02422d33a63b2e7a0696ff0305
hsrazna <anzar.terek@gmail.com>
2017-04-10 10:55:14 +0300

    list

---
+++ app/include/list.php
@@ -0,0 +1,57 @@
+<section class="az-list">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2><span>Список коттеджных поселков</span></h2>
+				<div class="az-mb10"><a href="#" class="az-link az-go-back-link">Вернуться к списку</a></div>
+				<div id="tabs">
+					<ul>
+						<li><a href="#tabs-1">Список коттеджных поселков</a></li>
+						<li><a href="#tabs-2">Ценовые категории</a></li>
+					</ul>
+					<div id="tabs-1">
+						<div class="az-list-tab1">
+							<div class="az-list-title"><span>Массовое изменение участков</span></div>
+							<div class="az-list-table">
+								<table>
+									<tr>
+										<td>1</td>
+										<td><a href="#">Ракино</a></td>
+										<td>Статус: Черновик</td>
+									</tr>
+									<tr>
+										<td>2</td>
+										<td><a href="#">Простоквашино</a></td>
+										<td>Статус: Черновик</td>
+									</tr>
+									<tr>
+										<td>3</td>
+										<td><a href="#">Ракино</a></td>
+										<td>Статус: Черновик</td>
+									</tr>
+									<tr>
+										<td>4</td>
+										<td><a href="#">Простоквашино</a></td>
+										<td>Статус: Черновик</td>
+									</tr>
+									<tr>
+										<td>5</td>
+										<td><a href="#">Ракино</a></td>
+										<td>Статус: Черновик</td>
+									</tr>
+									<tr>
+										<td>6</td>
+										<td><a href="#">Простоквашино</a></td>
+										<td>Статус: Черновик</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+					<div id="tabs-2">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>	
